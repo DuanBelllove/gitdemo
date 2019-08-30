e478a602c4242806dc8854d1d39a7d3fa9f08722
dxh <belllove@163.com>
2019-08-30 09:25:46 +0800

    修改代码,模拟其他程序员上传代码

--- gitdemo/src/main/java/it/cast/gitdemo/Test.java
+++ gitdemo/src/main/java/it/cast/gitdemo/Test.java
@@ -6,8 +6,10 @@ public class Test {
         System.out.println("git第一次测试");
         System.out.println("git第一次测试");
         System.out.println("git第一次测试");
+        System.out.println("git第二次测试");
         System.out.println("git第一次测试");
         System.out.println("git第一次测试");
+        System.out.println("git第三次测试");
         System.out.println("git第一次测试");
     }
 }
