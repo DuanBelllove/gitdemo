0cf331427f9bdbad91c8832025526375135d98e5
dxh <belllove@163.com>
2019-08-30 09:19:41 +0800

    创建gitdemo

---
+++ gitdemo/src/main/java/it/cast/gitdemo/Test.java
@@ -0,0 +1,13 @@
+package it.cast.gitdemo;
+
+public class Test {
+
+    public static void main(String[] args) {
+        System.out.println("git第一次测试");
+        System.out.println("git第一次测试");
+        System.out.println("git第一次测试");
+        System.out.println("git第一次测试");
+        System.out.println("git第一次测试");
+        System.out.println("git第一次测试");
+    }
+}
